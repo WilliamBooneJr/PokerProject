e11ed5c488f062fda817c112bc6e6e7d36383545
Thomas Putnam <50059795+theyungestputt@users.noreply.github.com>
2023-10-13 11:27:48 -0600

    Update pokeranalysis.py

--- pokeranalysis.py
+++ pokeranalysis.py
@@ -1,4 +1,25 @@
 import stdio
 import stdarray
+import random
+import stdio
+import sys
 
-stdio.writeln('Test')
+#Creates an array of all suits and array of all card ranks, cocoantes these arrays to create a full deck
+SUITS = ['Clubs', 'Diamonds', 'Hearts', 'Spades']
+RANKS = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+deck = []
+for suit in SUITS:
+    for rank in RANKS:
+        card = rank + ' of ' + suit
+        deck += [card]
+
+#Randomnly iterates and prints 5 cards for every n poker hands
+hand = []
+n = int(sys.argv[1])
+for i in range(n):
+    stdio.writeln('Hand Number ' + str(i+1) + ' is..')
+    for i in range(0,5):
+        x = random.randrange(0, 52)
+        stdio.writeln(deck[x])
+        hand += [x] 
+    stdio.writeln()
